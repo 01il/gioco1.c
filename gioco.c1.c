721fc8ba1f22003d8bdf45cfbf9663746f6e32a8
01il <161503030+01il@users.noreply.github.com>
2024-03-07 15:13:13 +0100

    Add files via upload

---
+++ gioco.c1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char stringa1[20] = {'\0'};
+    char stringa2[] = "Ciao";
+    printf("A>> Start game 'Ciao':");
+    printf("B>> Return game");
+    printf("C>> Exit");
+    'scanf= ("%d ; &d")';
+
+
+    // if(stringa1 == stringa2; char=20, scanf=0):
+    // if(char1=20 , char2= ciao):
+    if(i == 'C' ||  i == 'c') printf("%s\n", stringa2);
+    return 0;
+}
